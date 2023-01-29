7c151098d62d2d9cc9d823ad464293a0d862d585
min <wp2102@kakao.com>
2023-01-30 01:09:52 +0900

    RTSP update

---
+++ Views/script.js
@@ -0,0 +1,9 @@
+// window.onload=function (){
+//     var canvas = document.getElementById("my_canvas").getContext("2d");
+//     var canvas1 = document.getElementById("my_canvas1").getContext("2d");
+//
+//     canvas.fillStyle = "green"; // 색상명
+//     canvas.fillRect(0,0,4002,3002);
+//     canvas1.fillStyle = "red"; // 색상명
+//     canvas1.fillRect(0,0,7882,10802);
+// }
